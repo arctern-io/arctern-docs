e7851168ba28fb7f5e4ec1a60740ec3844c4ee80
become-nice <56624819+become-nice@users.noreply.github.com>
2020-04-24 10:31:55 +0800

    Clean locale file and modify compile.py to avoid manually add code in sphinx_build. (#22)
    * adjust doc catalog struct
    
    * adjust doc catalog struct
    
    * adjust doc catalog struct
    
    * add zh-cn comment
    
    * fix conflicts
    
    * xxx
    
    * xxx
    
    * add locale
    
    * add cn comment
    
    * xxx
    
    * add cn doc
    
    * fix conflicts
    
    * alter doc directory
    
    * delete modules.rst
    
    * modified markdown file
    
    * fix cn comment
    
    * modified cn comment
    
    * add plot api comment
    
    * fix conflicts
    
    * Add register_func comment
    
    * modify docs
    
    * fix conflicts
    
    * adjust project directory
    
    * add po file
    
    * xxx
    
    * delete some po file
    
    * clean locale file

--- doc-cn/compile.py
+++ doc-cn/compile.py
@@ -9,11 +9,37 @@ def replace(source_api_path,src_prefix,dst_prefix):
            with open(source_api_path + os.sep + html_file, 'w') as f:
                 f.write(contents)
 
+def add_content(path):
+    file_read = open(path, 'r')
+    udf_file_read = open('replace_udf.py','r')
+    content = file_read.read()
+    contentadd = udf_file_read.read()
+    pos = content.find("if __name__ == '__main__':")
+    file_read.close()
+    udf_file_read.close()
+    file_write = open(path, 'w')
+    content = content[:pos] + contentadd + content[pos:]
+    file_write.write(content)
+    file_write.close()
+
+def delete_content(path):
+    file_read = open(path, 'r')
+    content = file_read.read()
+    pos_begin = content.find("import functools")
+    pos_end = content.find("if __name__ == '__main__':")
+    file_write = open(path, 'w')
+    content = content[:pos_begin] + content[pos_end:]
+    file_write.write(content)
+    file_write.close()
+
 if __name__ == "__main__":
+    path = os.popen('which sphinx-build').readline().replace("\n", "")
+    add_content(path)
     os.system('make clean')
     os.system('make html')
     os.system('make gettext')
     os.system('make html')
+    delete_content(path)
 
     source_api_path = 'build/html/python/api/geospatial/function/aggr'
     src_prefix = '>arctern._wrapper_func.<'
